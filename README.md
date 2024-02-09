2010e24b687084d53416560a5d263843751b5d05
Julien Pajot <julien.pajot@supdevinci-edu.fr>
2024-02-09 12:36:14 +0100

    utilisation de methode existante

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Tp 1 : c'est extrêmement long et redondant
 Tp 2 : Le nombre de ligne est bien plus court et conci 
 Tp 3 : Implementation de getter et de setter grace au alt insert 
+Tp 4 : Utilisation de Parse et du max 
---
+++ approche-objet/src/utils/ConversionNombre.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class ConversionNombre {
+    public static void main(String[] args) {
+        String number = "5";
+        int result = Integer.parseInt(number);
+
+        int a = 4;
+        int b = 5;
+
+        int max = Integer.max(a,b);
+
+        System.out.println(max);
+    }
+}
