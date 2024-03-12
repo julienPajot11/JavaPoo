6149319e5db46779159e7639d788c199668ccff2
Julien Pajot <julien.pajot@supdevinci-edu.fr>
2024-03-12 12:08:19 +0100

    Début interaction BDD

---
+++ Test-Jpa/src/main/java/sdv-12-03-2024/App.java
@@ -0,0 +1,14 @@
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+
+public class App {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
+        EntityManager em = entityManagerFactory.createEntityManager();
+        entityManagerFactory.close();
+        em.close();
+
+    }
+}
