a10de3693ae5f99c3cea168ba3e8d52652ca3909
Julien Pajot <julien.pajot@supdevinci-edu.fr>
2024-03-12 12:20:20 +0100

    project renew

--- JPA/src/main/java/sdvClass2024/Main.java
+++ JPA/src/main/java/sdvClass2024/Main.java
@@ -1,14 +1,14 @@
+package sdvClass2024;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
-
-public class App {
+public class Main {
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
         EntityManager em = entityManagerFactory.createEntityManager();
         entityManagerFactory.close();
         em.close();
-
     }
 }
