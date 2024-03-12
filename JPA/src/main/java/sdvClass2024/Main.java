45a3a3f5b2b440576345741f9051a2d98e6aa452
Julien Pajot <julien.pajot@supdevinci-edu.fr>
2024-03-12 14:02:47 +0100

    CRUD for data

--- JPA/src/main/java/sdvClass2024/Main.java
+++ JPA/src/main/java/sdvClass2024/Main.java
@@ -1,13 +1,42 @@
 package sdvClass2024;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.*;
+import sdvClass2024.Model.Livre;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
         EntityManager em = entityManagerFactory.createEntityManager();
+        // R of CRUD
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+            Livre getLivre = em.find(Livre.class, 1);
+            if (getLivre != null){
+                System.out.println(getLivre);
+            }
+
+        // C of CRUD
+        /*Livre newlivre = new Livre("3 petits cochon","le grand méchant loup");
+            em.persist(newlivre);*/
+
+        // U of CRUD
+        Livre livre = em.find(Livre.class, 6);
+        if (livre != null){
+            livre.setAuteur("Cendrillon");
+        }
+        //D of Delete
+        Livre removeLivre = em.find(Livre.class, 7);
+        if (removeLivre != null){
+            em.remove(removeLivre);
+        }
+
+        // R of CRUD pour avoir la liste des livres
+        Query query2 = em.createQuery("select l from Livre l");
+            System.out.println(query2.getResultList().toString());
+        et.commit();
+        em.close();
         entityManagerFactory.close();
         em.close();
     }
---
+++ JPA/src/main/java/sdvClass2024/Model/Livre.java
@@ -0,0 +1,76 @@
+package sdvClass2024.Model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name="livre")
+public class Livre implements Serializable {
+
+
+
+    @Id
+    @Column(name="ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name="TITRE")
+    private String titre;
+    @Column(name="AUTEUR")
+    private String auteur;
+
+    public Livre() {
+    }
+
+    public Livre(String titre, String auteur) {
+        this.titre = titre;
+        this.auteur = auteur;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livre livre = (Livre) o;
+        return Objects.equals(id, livre.id) && Objects.equals(titre, livre.titre) && Objects.equals(auteur, livre.auteur);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, titre, auteur);
+    }
+
+    @Override
+    public String toString() {
+        return "Livre{" +
+                "id=" + id +
+                ", titre='" + titre + '\'' +
+                ", auteur='" + auteur + '\'' +
+                '}';
+    }
+}
